d2ab6eef68907009ee1c01827cc392b1b240c406
vishishtapakhi <149595288+vishishtapakhi@users.noreply.github.com>
2024-11-07 16:01:16 +0530

    Add files via upload

---
+++ resources/static/js/ind.js
@@ -0,0 +1,36 @@
+// Sidebar toggle
+const sidebarToggle = document.querySelector("#sidebar-toggle");
+sidebarToggle.addEventListener("click", function() {
+    document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+// Theme toggle
+document.querySelector(".theme-toggle").addEventListener("click", () => {
+    toggleLocalStorage();
+    toggleRootClass();
+});
+
+function toggleRootClass() {
+    const current = document.documentElement.getAttribute('data-bs-theme') || 'light'; // default to 'light'
+    const inverted = current === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme', inverted);
+}
+
+function toggleLocalStorage() {
+    if (isLight()) {
+        localStorage.removeItem("light");
+    } else {
+        localStorage.setItem("light", "set");
+    }
+}
+
+function isLight() {
+    return localStorage.getItem("light");
+}
+
+// Apply the theme on page load
+if (isLight()) {
+    document.documentElement.setAttribute('data-bs-theme', 'light');
+} else {
+    document.documentElement.setAttribute('data-bs-theme', 'dark');
+}
